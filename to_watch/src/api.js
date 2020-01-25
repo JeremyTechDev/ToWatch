8f64f4706b6be63b3614ad65b1a8e6fc58f3629d
Jeremy Munoz Torres <jermy2918@gmail.com>
2020-01-24 20:52:07 -0400

    Changed de date format for the start date provided by the API

--- to_watch/src/api.js
+++ to_watch/src/api.js
@@ -7,6 +7,28 @@ export function fetchTVShows(page = 1) {
   return fetch(endpoint)
     .then(res => res.json())
     .then(data => {
-      return data.tv_shows
+      const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ];
+
+      //converts the YYYY-MM-DD date format to MM YYYY format
+      for (let i in data.tv_shows) {
+        let date = data.tv_shows[i].start_date;
+        date = new Date(date);
+        date = months[date.getMonth()] + " " + date.getFullYear();
+      }
+
+      return data.tv_shows;
     });
 }
