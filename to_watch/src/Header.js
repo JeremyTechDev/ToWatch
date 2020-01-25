ac8ef83839949c76083a8abfd2648fd46fda5765
Jeremy Munoz Torres <jermy2918@gmail.com>
2020-01-25 18:38:41 -0400

    Created method for the searchBar so it now can fetch the user's input

--- to_watch/src/Header.js
+++ to_watch/src/Header.js
@@ -2,6 +2,29 @@ import React from "react";
 import "./SASS/header.scss";
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchBar: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    //API
+    alert(this.state.searchBar)
+  }
+
+  //Saves avery change of the search-bar on the state
+  handleChange = event => {
+    this.setState({
+      searchBar: event.target.value
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -11,8 +34,14 @@ export default class Header extends React.Component {
           </div>
 
           <div className="search-bar">
-            <form>
-              <input type="text" className="bar" placeholder="Find me..." />
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                className="bar"
+                onChange={this.handleChange}
+                value={this.state.searchBar}
+                placeholder="Find me..."
+              />
               <button title="Search" type="submit" className="bar-btn">
                 <i class="fas fa-search" />
               </button>
